99ebcb09bc4621833a97a055d516d07475d6ae0e
Fa'iq Fadlurrohman <faiqfadlurrohman1002@gmail.com>
2026-06-04 07:59:48 +0700

    Menambahkan file coba.js dan index.js. Dan menambahkan file index.html, satu.js, dan dua.js ke dalam folder test

---
+++ index.js
@@ -0,0 +1,8 @@
+// const nama = "Faiq Fadlurrohman";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+const cetakNama = require("./coba");
+
+// console.log("Hello!");
+console.log(cetakNama("Faiq"));
