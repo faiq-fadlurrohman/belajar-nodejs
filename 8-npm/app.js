78f46b222ebf80d067302320ffac41cf1d3f6281
Fa'iq Fadlurrohman <faiqfadlurrohman1002@gmail.com>
2026-06-13 09:02:50 +0700

    Mengubah file app.js. Dan menginstall package chalk.

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('faiq@gmail.c'));
+// console.log(validator.isMobilePhone('07223456789', 'id-ID'));
+// console.log(validator.isNumeric('07223456789a'));
+
+// console.log(chalk.italic.bgBlue.black("Hello World!"));
+const nama = 'Faiq';
+const pesan = chalk`Lorem ipsum dolor {bgGreen.black.bold sit amet} consectetur {bgGreen.italic.black adipisicing} elit. Velit, veritatis. Nama saya : ${nama}`;
+console.log(chalk(pesan));
