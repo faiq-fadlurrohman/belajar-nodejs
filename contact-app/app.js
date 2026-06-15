1446d265f225135de203c678e7176a12091bdffe
Fa'iq Fadlurrohman <faiqfadlurrohman1002@gmail.com>
2026-06-15 09:13:58 +0700

    Menambahkan app.js ke /contact-app. Dan menambahkan contacts.json ke /contact-app/data

---
+++ contact-app/app.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout 
+});
+
+// Membuat folder data jika belum ada
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+}
+
+// Membuat file contacts.json jika belum ada
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+    fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const tulisPertanyaan = (pertanyaan) => {
+    return new Promise((resolve, reject) => {
+        rl.question(pertanyaan, (nama) => {
+            resolve(nama);
+        });
+    });
+}
+
+const main = async () => {
+    const nama = await tulisPertanyaan('Masukkan nama anda : ');
+    const email = await tulisPertanyaan('Masukkan email anda : ');
+    const noHP = await tulisPertanyaan('Masukkan nomor HP anda : ');
+
+    const contact = {nama, email, noHP};
+    const file = fs.readFileSync('data/contacts.json', 'utf-8');
+    const contacts = JSON.parse(file);
+
+    contacts.push(contact);
+    
+    fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+    console.log('Terimkasih sudah memasukkan data.');
+            
+    rl.close();
+}
+
+main();
